c45e8351103c329483207f9851ce5f1f310f308c
hackpres <102390515+hackpres@users.noreply.github.com>
2023-04-14 11:04:51 -0400

    created and rendered D6 component which is a 6 sided 3d die.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import D6 from './components/D6';
 
 function App() {
   return (
     <div className="App">
       <h1>Application</h1>
+      <D6 />
     </div>
   );
 }
---
+++ client/src/components/D6/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./d6style.css";
+
+function D6() {
+  return (
+      <div className='d6-wrapper'>
+          {/* <div className='d6-spin'> */}
+            <div className='side one'></div>
+            <div className='side two'></div>
+            <div className='side three'></div>
+            <div className='side four'></div>
+            <div className='side five'></div>
+            <div className='side six'></div>
+          {/* </div> */}
+    </div>
+  )
+}
+
+export default D6
